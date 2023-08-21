5a15e55cf35075ae8829616cca228ba285c0a821
Vale20m <valentinveintemilla@gmail.com>
2023-08-21 20:56:38 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // Guardamos la casilla de texto y el boton en una variable
 
 const boton = document.querySelector('#buttonText');
-const texto = document.querySelector('#inputText')
+const texto = document.querySelector('#inputText');
 
 // Definimos un evento para que al presionar el botón se guarde en "localStorage" lo que ingresamos en la casilla de texto
 
